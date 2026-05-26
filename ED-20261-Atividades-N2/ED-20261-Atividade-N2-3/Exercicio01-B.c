21a2a09d4d9c0a23d00cf77db8496aa8e339fe9f
Kauã Sato Sposato <131924519+SposatoDev@users.noreply.github.com>
2026-05-26 17:31:53 -0300

    Update Exercicio01-B.c
    Added functionality to calculate the sum of an array and invert its elements using pointer arithmetic.

--- ED-20261-Atividades-N2/ED-20261-Atividade-N2-3/Exercicio01-B.c
+++ ED-20261-Atividades-N2/ED-20261-Atividade-N2-3/Exercicio01-B.c
@@ -1,1 +1,43 @@
-printf("Hello, World!")
+// RA: 2040482513047 - Kauã Sato Sposato
+
+#include <stdio.h>
+
+int main() {
+    int v[] = {10, 20, 30, 40, 50};
+
+    int *inicio = v;
+    int *fim = v + 4;
+
+    int *p;
+    int soma = 0;
+
+    printf("Vetor original:\n");
+
+    for (p = v; p < v + 5; p++) {
+        printf("Valor: %d\n", *p);
+        printf("Endereco: %p\n", p);
+
+        soma += *p;
+    }
+
+    printf("\nSoma = %d\n", soma);
+
+    while (inicio < fim) {
+        *inicio = *inicio + *fim;
+        *fim = *inicio - *fim;
+        *inicio = *inicio - *fim;
+
+        inicio++;
+        fim--;
+    }
+
+    printf("\nVetor invertido:\n");
+
+    for (p = v; p < v + 5; p++) {
+        printf("%d ", *p);
+    }
+
+    printf("\n");
+
+    return 0;
+}
